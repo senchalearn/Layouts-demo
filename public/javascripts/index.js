81f5e77233478a9e50bb9da058bff1a182e91e47
Drew Neil <andrew.jr.neil@gmail.com>
2012-01-23 15:54:51 +0100

    Go from onReady() to application launch().

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,48 +1,41 @@
-// Panels can act as components (i.e. content) or as a container for other 
-// components. So it is possible to mix and match the layout types.
-// Here, a vbox layout contains 3 compenents, each of which is a carousel.
+Ext.Loader.setConfig({enabled: true});
 
-Ext.setup({
-    onReady: function() {
+Ext.application({
+    name: 'App',
 
-        var topChanger = new Ext.Carousel({
-            items: [
-                {cls: "head-1 head"},
-                {cls: "head-2 head"},
-                {cls: "head-3 head"},
-            ]
-        });
-
-        var centerChanger = new Ext.Carousel({
-            items: [
-                {cls: "torso-1 torso"},
-                {cls: "torso-2 torso"},
-                {cls: "torso-3 torso"},
-            ]
-        });
-
-        var bottomChanger = new Ext.Carousel({
-            items: [
-                {cls: "legs-1 legs"},
-                {cls: "legs-2 legs"},
-                {cls: "legs-3 legs"},
-            ]
-        });
-
-        var credits = new Ext.Panel({
-            docked: 'bottom',
-            html: 'Illustrations by <a href="http://www.veryworrying.com/">Laurie Pink</a>'
-        });
-
-        new Ext.Panel({
-            fullscreen: true,
+    launch: function() {
+        Ext.Viewport.add({
             layout: {
                 type: 'vbox',
                 align: 'stretch'
             },
-            defaults: {flex: 1},
-            items: [topChanger, centerChanger, bottomChanger, credits]
+            defaults: {
+                flex: 1,
+                xtype: 'carousel'
+            },
+            items: [{
+                items: [
+                    {cls: "head-1 head"},
+                    {cls: "head-2 head"},
+                    {cls: "head-3 head"},
+                ]
+            }, {
+                items: [
+                    {cls: "torso-1 torso"},
+                    {cls: "torso-2 torso"},
+                    {cls: "torso-3 torso"},
+                ]
+            }, {
+                items: [
+                    {cls: "legs-1 legs"},
+                    {cls: "legs-2 legs"},
+                    {cls: "legs-3 legs"},
+                ]
+            }, {
+                xtype: 'component',
+                docked: 'bottom',
+                html: 'Illustrations by <a href="http://www.veryworrying.com/">Laurie Pink</a>'
+            }]
         });
-
     }
 });
